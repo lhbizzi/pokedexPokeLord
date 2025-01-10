6f63875fcb567070a9d65ae30a430006d25c8af3
lhbizzi <106413189+lhbizzi@users.noreply.github.com>
2025-01-10 10:05:32 -0300

    Normalize line endings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,35 +6,46 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Pokecard from "../components/pokecard";
 import { Skeletons } from "../components/Skeletons";
+import pLimit from "p-limit";
 
 export const Home = ({ setPokemonData }) => {
   const [pokemons, setPokemons] = useState([]);
+  const [allPokemons, setAllPokemons] = useState([]); // Armazena todos os pokémons para filtros
   const navigate = useNavigate();
 
   useEffect(() => {
     getPokemons();
   }, []);
 
-  const getPokemons = () => {
-    var endpoints = [];
-    for (var i = 1; i < 1026; i++) {
-      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+  const getPokemons = async () => {
+    const limit = pLimit(10); // Limitar para 10 requisições simultâneas
+    const endpoints = Array.from(
+      { length: 650 },
+      (_, i) => `https://pokeapi.co/api/v2/pokemon/${i + 1}/`
+    );
+
+    try {
+      const results = await Promise.all(
+        endpoints.map((endpoint) => limit(() => axios.get(endpoint)))
+      );
+
+      const pokemonsData = results.map((res) => res.data);
+      setPokemons(pokemonsData);
+      setAllPokemons(pokemonsData); // Armazena todos os pokémons para filtros
+    } catch (error) {
+      console.error("Erro ao buscar pokémons:", error);
     }
-    axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
   };
 
   const pokemonFilter = (name) => {
-    var filteredPokemons = [];
     if (name === "") {
-      getPokemons();
+      setPokemons(allPokemons); // Restaura todos os pokémons
+    } else {
+      const filteredPokemons = allPokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(name.toLowerCase())
+      );
+      setPokemons(filteredPokemons);
     }
-    for (var i in pokemons) {
-      if (pokemons[i].data.name.includes(name)) {
-        filteredPokemons.push(pokemons[i]);
-      }
-    }
-
-    setPokemons(filteredPokemons);
   };
 
   const pokemonPickHandler = (pokemonData) => {
@@ -45,19 +56,25 @@ export const Home = ({ setPokemonData }) => {
   return (
     <div>
       <Navbar pokemonFilter={pokemonFilter} />
-            <Container maxWidth="false">
-                <Grid container>
-                    {pokemons.length === 0 ? <Skeletons /> :
-                        pokemons.map((pokemon, key) => (
-                            <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
-                                <Box onClick={() => pokemonPickHandler(pokemon.data)}>
-                                    <Pokecard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types} />
-                                </Box>
-                            </Grid>
-                        ))
-                    }
-                </Grid>
-            </Container>
+      <Container maxWidth="false">
+        <Grid container spacing={2}>
+          {pokemons.length === 0 ? (
+            <Skeletons />
+          ) : (
+            pokemons.map((pokemon, key) => (
+              <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
+                <Box onClick={() => pokemonPickHandler(pokemon)}>
+                  <Pokecard
+                    name={pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                    image={pokemon.sprites.front_default}
+                    types={pokemon.types}
+                  />
+                </Box>
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </Container>
     </div>
   );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,69 +1,172 @@
-import React, { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import { Box, Chip, Container, Divider, Paper, Typography } from '@mui/material';
-import PokeTable from '../components/PokeTable';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import {
+  Box,
+  Chip,
+  Container,
+  Divider,
+  Paper,
+  Typography,
+} from "@mui/material";
+import PokeTable from "../components/PokeTable";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import indisponível from '../assets/indisponivel.png';
 
 export const Profile = ({ pokemonData }) => {
-    const { name, sprites, moves } = pokemonData || {};
-    const navigate = useNavigate();
+  const { name, sprites, moves, id } = pokemonData || {};
+  const navigate = useNavigate();
+  const [ animation, setAnimation ] = useState(null);
 
-    useEffect(() => {
-        if (!pokemonData) {
-            navigate('/');
-        }
-    }, [navigate, pokemonData]); // Incluindo 'navigate' e 'pokemonData' no array de dependências
+  useEffect(() => {
+    if (!pokemonData) {
+      navigate("/");
+    }
+  }, [navigate, pokemonData]);
 
-    if (!pokemonData) return null;
+  if (!pokemonData) return null;
 
-    return (
-        <>
-            <Navbar hideSearch/>
-            <Container maxWidth="md">
-                <Paper elevation={3}>
-                    <Box display="flex" justifyContent="center" alignItems="center" p={5} flexDirection="column">
-                        <Typography variant="h4">{name}</Typography>
-                        <Box display="flex" m={5} width={'100%'} alignItems={'center'} justifyContent={'center'} marginBottom="15px" sx={{flexDirection: {
-                            xs: 'column',
-                            md: 'row'
-                        }}}>
-                            <Box component='img' src={sprites.front_default} width={'100%'} height={'100%'}/>
-                            <Box component='img' src={sprites.back_default} width={'100%'} height={'100%'}/>
-                            <PokeTable pokemonData={pokemonData}/>
-                        </Box>
-                        <Box width={'100%'} alignItems={'center'} justifyContent={'center'}>
-                            <Divider>Variações</Divider>
-                            <Box display='flex' justifyContent='space-between'>
-                                <Box component='img' src={sprites.front_shiny} width={'30%'} height={'30%'}/>
-                                <Box component='img' src={sprites.front_female} width={'30%'} height={'30%'}/>
-                                <Box component='img' src={sprites.front_shiny_female} width={'30%'} height={'30%'}/>
-                            </Box>
-                            <Box display='flex' justifyContent='space-between'>
-                                <Box component='img' src={sprites.back_shiny} width={'30%'} height={'30%'}/>
-                                <Box component='img' src={sprites.back_female} width={'30%'} height={'30%'}/>
-                                <Box component='img' src={sprites.back_shiny_female} width={'30%'} height={'30%'}/>
-                            </Box>
-                            <Box display='flex' justifyContent='space-between' marginBottom={'40px'} marginLeft={'15px'} >
-                                <Box marginLeft={'40px'}>
-                                    <Typography>Versão Shiny</Typography>
-                                </Box>
-                                <Box marginLeft={'40px'}>
-                                    <Typography>Versão Feminina</Typography>
-                                </Box>
-                                <Box marginRight={'40px'}>
-                                    <Typography>Versão Feminina Shiny</Typography>
-                                </Box>
-                            </Box>
-                            <Divider>Habilidades</Divider>
-                            <Box textAlign={'center'} marginTop='15px'>
-                                {moves.map((moveData, key) =>
-                                    <Chip key={key} sx={{ margin: '5px' }} label={moveData.move.name} />
-                                )}
-                            </Box>
-                        </Box>
-                    </Box>
-                </Paper>
-            </Container>
-        </>
-    );
-}
+  let nameInfo = null;
+  let nameInfo1 = null;
+
+  let respGif = null;
+
+  axios
+    .get(
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`,
+    )
+    .then(() => {setAnimation(true);})
+    .catch(() => setAnimation(null));
+
+  if (animation) {
+    respGif = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`;
+  }
+  else {
+    respGif = indisponível;
+  }
+
+  return (
+    <>
+      <Navbar hideSearch />
+      <Container maxWidth="md">
+        <Paper elevation={3}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            p={5}
+            flexDirection="column"
+          >
+            <Typography variant="h4">{name}</Typography>
+            <Box
+              display="flex"
+              m={5}
+              width={"100%"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              marginBottom="15px"
+              sx={{
+                flexDirection: {
+                  xs: "column",
+                  md: "row",
+                },
+              }}
+            >
+              <Box
+                component="img"
+                src={sprites.front_default}
+                width={"100%"}
+                height={"100%"}
+              />
+              <Box
+                component="img"
+                src={sprites.back_default}
+                width={"100%"}
+                height={"100%"}
+              />
+              <PokeTable pokemonData={pokemonData} />
+            </Box>
+            <Box width={"100%"} alignItems={"center"} justifyContent={"center"}>
+              <Divider>Variações</Divider>
+              <Box display="flex" justifyContent="space-between">
+                <Box
+                  component="img"
+                  src={sprites.front_shiny}
+                  width={"30%"}
+                  height={"30%"}
+                />
+                <Box
+                  component="img"
+                  src={sprites.front_female}
+                  width={"30%"}
+                  height={"30%"}
+                />
+                <Box
+                  component="img"
+                  src={sprites.front_shiny_female}
+                  width={"30%"}
+                  height={"30%"}
+                />
+              </Box>
+              <Box display="flex" justifyContent="space-between">
+                <Box
+                  component="img"
+                  src={sprites.back_shiny}
+                  width={"30%"}
+                  height={"30%"}
+                />
+                <Box
+                  component="img"
+                  src={sprites.back_female}
+                  width={"30%"}
+                  height={"30%"}
+                />
+                <Box
+                  component="img"
+                  src={sprites.back_shiny_female}
+                  width={"30%"}
+                  height={"30%"}
+                />
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                marginBottom={"40px"}
+                marginLeft={"15px"}
+              >
+                <Box marginLeft={"40px"}>
+                  <Typography>Versão Shiny</Typography>
+                </Box>
+                <Box marginLeft={"40px"}>
+                  <Typography>{nameInfo}</Typography>
+                </Box>
+                <Box marginRight={"40px"}>
+                  <Typography>{nameInfo1}</Typography>
+                </Box>
+              </Box>
+              <Divider>Animação</Divider>
+              <Box display="flex" justifyContent={"center"} alignItems={"center"} margin={"30px"}>
+                <Box
+                  component="img"
+                  src={respGif}
+                  width={"50%"}
+                  height={"50%"}
+                />
+              </Box>
+              <Divider>Habilidades</Divider>
+              <Box textAlign={"center"} marginTop="15px">
+                {moves.map((moveData, key) => (
+                  <Chip
+                    key={key}
+                    sx={{ margin: "5px" }}
+                    label={moveData.move.name}
+                  />
+                ))}
+              </Box>
+            </Box>
+          </Box>
+        </Paper>
+      </Container>
+    </>
+  );
+};
