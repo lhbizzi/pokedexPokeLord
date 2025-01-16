8bb626319ad1c7fa9dd3f1d14e5cc0334b094dd8
lhbizzi <106413189+lhbizzi@users.noreply.github.com>
2025-01-16 12:54:59 -0300

    fixing_errors

--- src/components/ScrollToTop/index.jsx
+++ src/components/ScrollToTop/index.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const ScrollToTop = () => {
   return (
     <Box sx={{ '& > :not(style)': { m: 1 }, position: 'fixed', alignContent: 'center', bottom: 20, right: 20, zIndex: 9999 }}>
-      <Fab color="primary" aria-label="add" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+      <Fab color="primary" aria-label="add" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{ backgroundColor: '#0e70b7' }}>
         <SlArrowUp />
       </Fab>
     </Box>
